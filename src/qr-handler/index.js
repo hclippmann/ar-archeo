de2251e6a32b621c5f7db4b89a30f25dfc0fbad4
lukasz-madej <lukasz.madej@kreuzwerker.pl>
2018-09-25 10:00:24 +0200

    Adds appUrl to config

--- src/qr-handler/index.js
+++ src/qr-handler/index.js
@@ -1,9 +1,10 @@
 import QRCode from 'qrcode';
+import config from '../../static/config'
 
 export default class QrHandler {
   generateQrCodeToCanvas = (canvas, marker) => {
     QRCode.toCanvas(canvas, marker.location.url);
   };
 
-  generateQrCodeToUrl = (marker) => QRCode.toDataURL(`https://kreuzwerker.github.io/ar-archeo/?markerId=${marker.id}`);
+  generateQrCodeToUrl = (marker) => QRCode.toDataURL(`${config.appUrl}?markerId=${marker.id}`);
 }
